f1eda7af3b69045a7dc5e97d200df9c9b0a2ffe2
Armando Martinez <alemar.martinez16@gmail.com>
2024-02-21 12:45:35 -0300

    refactor: Update FormPage component to handle form submission Description: Refactored the 'FormPage.jsx' component to handle form submission using fetch API. Implemented an onSubmit event handler to prevent the default form submission behavior and handle the submission asynchronously. Extracted form data using FormData API and constructed an SMS object containing phone number and message. Utilized the fetch API to send a POST request to the '/api/sms' endpoint with the SMS data in JSON format. Upon receiving a response from the server, displayed a success message and logged the response data to the console. Adjusted form input IDs and placeholders for consistency and accessibility. These changes enhance the functionality of the FormPage component by enabling the sending of SMS messages through the application.

--- src/app/fomrPage/page.jsx
+++ src/app/fomrPage/page.jsx
@@ -1,21 +1,45 @@
+"use client"
 import React from 'react'
 
-function FormPage
-() {
+function FormPage() {
+  const onSumit = async (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.target);
+
+    const sms = {
+      phone: formData.get("phone"),
+      message: formData.get("message"),
+    };
+    const res = await fetch("/api/sms", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sms),
+    });
+
+    const data = await res.json();
+    console.log(data);
+    alert(`Message sent to ${sms.phone} successfully!`);
+  }
+  
   return (
     <div className="flex justify-center items-center h-screen">
-      <form className="bg-slate-900 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
+      <form
+        onSubmit={onSumit} className="bg-slate-900 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
         <h1 className="text-white text-center text-2xl font-bold mb-5">
           Send an SMS
         </h1>
         <div className="mb-5">
           <label
-            htmlFor="phone" // Corregir aquí
+            htmlFor="phone" 
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Phone Number
           </label>
           <input
+            id="phone"
             type="tel"
             name="phone"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -31,11 +55,12 @@ function FormPage
             Message
           </label>
           <textarea
-            id="message" // Corregir aquí
+            id="message" 
             name="message"
             rows="4"
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Leave a comment..."
+            placeholder="Write your message here..."
+            required
           ></textarea>
         </div>
 
