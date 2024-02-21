5fd159146171b3a6f907b6c366d260ec37bf4ee9
Armando Martinez <alemar.martinez16@gmail.com>
2024-02-21 09:42:22 -0300

    refactor: Integrate Twilio API for sending SMS Description: - Updated the 'route.js' file to integrate Twilio API for sending SMS messages. - Added import statement for the 'twilio' library to facilitate interaction with the Twilio API. - Implemented code to utilize Twilio credentials stored in the environment variables for authentication. - Defined a GET function to send an SMS message using Twilio's client.messages.create() method. - Configured the message body, sender number, and recipient number using environment variables for flexibility and security. - Implemented error handling to catch any exceptions during the SMS sending process and provide appropriate responses. - These changes enhance the functionality of the application by enabling the sending of SMS messages via the Twilio API.

--- src/app/api/sms/route.js
+++ src/app/api/sms/route.js
@@ -1,5 +1,32 @@
 import { NextResponse } from "next/server";
+import twilio from "twilio";
 
-export function GET() {
-  return NextResponse.json({ message: "Hello world" });
+//credentials
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+//twilio client + credentials
+const client = twilio(accountSid, authToken);
+
+export async function GET() {
+  try {
+    //send SMS
+    const message = await client.messages.create({
+      body: "Hello from Twilio",
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: process.env.USER_PHONE_NUMBER,
+    });
+    console.log(message.sid);
+
+    return NextResponse.json({ message: "Hello world" });
+  } catch (error) {
+    console.log(error);
+
+    return (
+      NextResponse.json({ error: "Something went wrong, message not sent, please try again" }),
+      {
+        status: 500,
+      }
+    );
+  }
 }
