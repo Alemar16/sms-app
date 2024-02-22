e33feb6abb0850e1c9e136a0cdee9d7a0de8a7bd
Armando Martinez <alemar.martinez16@gmail.com>
2024-02-22 01:28:16 -0300

    style: Revamp HomePage layout and content
    Updated the HomePage component to feature a new layout and content structure. Reorganized elements to improve readability and visual appeal. Added a section wrapper with optimized spacing for better responsiveness. Enhanced text styling and added call-to-action buttons for improved user engagement. The FormPage component is now embedded within a redesigned section for better integration with the overall layout.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,17 +2,48 @@ import FormPage from "./fomrPage/page.jsx";
 
 function HomePage() {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-3xl font-bold mt-10 ">Welcome</h1>
+    <section className="sm:mt-16 lg:mt-20 mt-24 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 flex gap-10 lg:flex-justify lg:flex flex-col lg:flex-row">
+        <div className="sm:text-center lg:text-left">
+          <h1 className="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
+            <span className="block xl:inline">Data to enrich your</span>
+            <span className="block text-indigo-600 xl:inline">
+              {" "}
+              online business
+            </span>
+          </h1>
+          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
+          </p>
+          {/* Button Section */}
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow">
+              <a
+                href="#"
+                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-600 md:py-4 md:text-lg md:px-10"
+              >
+                Get started
+              </a>
+            </div>
+            <div className="mt-3 sm:mt-0 sm:ml-3">
+              <a
+                href="#"
+                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-gray-800 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+              >
+                Live demo
+              </a>
+            </div>
+          </div>
+        </div>
 
-      <div>
-         <FormPage />
-     </div>
-       
-     
-    </div>
-
-    
+        {/* Image Section */}
+        <div className="lg:inset-y-0 lg:right-0 lg:w-1/2 my-4">
+          <FormPage />
+        </div>
+      </div>
+    </section>
   );
 }
 
